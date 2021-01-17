6c372d51068ed62afe47ec3c8695edb36e8a74ea
Mhiho <m.pelka7@gmail.com>
2021-01-17 08:28:37 +0100

    styles correction

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -20,7 +20,7 @@ const Autocomplete : React.FC<IProps> = ({ value, onChange }) => {
             </form>
             <article>
                 <h3>Opis działania:</h3>
-                <h4>Aplikacja zaciąga userów, których lista jest następnie dostarczana do localStorage. Po kliknięciu na element wyrzucony z inputu, dodajemy ten element do drugiej listy. Usuwamy elementy z drugiej listy za pomocą kliknięcia w dany element.</h4>
+                <h4>Aplikacja zaciąga userów, których lista jest następnie dostarczana do localStorage. Po kliknięciu na element wyrzucony z inputu, dodajemy ten element do drugiej listy. Usuwamy elementy z drugiej listy za pomocą kliknięcia w dany element. Jeśli chodzi o mechanizm szukania, wg założenia, żeby nie instalować dodatkowych bibliotek (chodzi mi o lodasha) użyłem starej, dobrej pętli for.</h4>
             </article>
         </React.Fragment>
     )
